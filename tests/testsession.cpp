96113b3df1d71a886565ecd192b3f86994678685
Elvis Angelaccio <elvis.angelaccio@kdemail.net>
2015-07-16 15:51:31 +0200

    Implement Session unit test
    This requires a dummy static lib, since testsession.cpp needs also the Lap class (in
    addition to the Session class).
    
    At the moment testJson() fails.

---
+++ tests/testsession.cpp
@@ -0,0 +1,118 @@
+/*
+    Copyright (C) 2015 by Elvis Angelaccio <elvis.angelaccio@kdemail.net>
+
+    This file is part of Kronometer.
+
+    Kronometer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    Kronometer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kronometer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "session.h"
+#include "testsession.h"
+
+void TestSession::testDefaultSession()
+{
+    Session session;
+
+    QCOMPARE(session.time(), 0);
+    QVERIFY(session.isEmpty());
+    QVERIFY(not session.isOutdated());
+    QVERIFY(session.laps().isEmpty());
+    QVERIFY(session.name().isNull());
+    QVERIFY(session.note().isEmpty());
+}
+
+void TestSession::testTime()
+{
+    int time = 1000;
+    Session session(time);
+
+    QCOMPARE(session.time(), time);
+}
+
+void TestSession::testName()
+{
+    Session session;
+    auto test = QLatin1String("test");
+
+    session.setName(test);
+
+    QCOMPARE(session.name(), test);
+}
+
+void TestSession::testNote()
+{
+    Session session;
+    auto test = QLatin1String("test");
+
+    session.setNote(test);
+
+    QCOMPARE(session.note(), test);
+}
+
+void TestSession::testDate()
+{
+    auto date = QDateTime::currentDateTime();
+    Session session(0, date);
+
+    QCOMPARE(session.date(), date);
+}
+
+void TestSession::testLaps()
+{
+    Session session;
+
+    QList<Lap> laps = { Lap(QTime(1, 30)), Lap(QTime(2, 0)), Lap(QTime(1, 45)) };
+    foreach (const Lap& lap, laps) {
+        session.addLap(lap);
+    }
+
+    QCOMPARE(laps.size(), session.laps().size());
+}
+
+void TestSession::testEquality()
+{
+    auto date = QDateTime::currentDateTime();
+
+    Session session1(0, date);
+    Session session2(1000, date);
+
+    QCOMPARE(session1, session2);
+}
+
+void TestSession::testJson()
+{
+    Session session1(1000);
+    session1.setName(QStringLiteral("test-name"));
+    session1.setNote(QStringLiteral("test-note"));
+
+    QList<Lap> laps = { Lap(QTime(1, 30)), Lap(QTime(2, 0)), Lap(QTime(1, 45)) };
+    foreach (const Lap& lap, laps) {
+        session1.addLap(lap);
+    }
+
+    QJsonObject json;
+    session1.write(json);
+
+    Session session2 = Session::fromJson(json);
+
+    QEXPECT_FAIL("", "To be fixed ASAP", Continue);
+    QCOMPARE(session1, session2);
+
+    QCOMPARE(session1.time(), session2.time());
+    QCOMPARE(session1.name(), session2.name());
+    QCOMPARE(session1.note(), session2.note());
+    QCOMPARE(session1.laps().size(), session2.laps().size());
+}
+
+QTEST_MAIN(TestSession)
---
+++ tests/testsession.h
@@ -0,0 +1,41 @@
+/*
+    Copyright (C) 2015 by Elvis Angelaccio <elvis.angelaccio@kdemail.net>
+
+    This file is part of Kronometer.
+
+    Kronometer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    Kronometer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kronometer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TESTSESSION_H
+#define TESTSESSION_H
+
+#include <QtTest>
+
+class TestSession : public QObject
+{
+    Q_OBJECT
+
+private slots:
+
+    void testDefaultSession();
+    void testTime();
+    void testName();
+    void testNote();
+    void testDate();
+    void testLaps();
+    void testEquality();
+    void testJson();
+};
+
+#endif
