505816f8f39efc66e549bcf4f086faa0e0caaa62
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-29 20:21:33 +0200

    Convert license headers to SPDX

--- src/models/lapmodel.cpp
+++ src/models/lapmodel.cpp
@@ -1,20 +1,9 @@
 /*
-    Copyright (C) 2014 by Elvis Angelaccio <elvis.angelaccio@kde.org>
+    SPDX-FileCopyrightText: 2014 Elvis Angelaccio <elvis.angelaccio@kde.org>
 
     This file is part of Kronometer.
 
-    Kronometer is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
-    (at your option) any later version.
-
-    Kronometer is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Kronometer.  If not, see <http://www.gnu.org/licenses/>.
+    SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 #include "lapmodel.h"
