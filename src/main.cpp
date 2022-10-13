bafa17f45af437cd3d490fd42ac0b41e0f340dbb
Laurent Montel <montel@kde.org>
2022-10-13 07:56:52 +0200

    It's enabled by default in qt6

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,10 @@
 int main (int argc, char **argv)
 {
     KLocalizedString::setApplicationDomain("kronometer");
-
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+#endif
     QApplication app {argc, argv};
 
     KCrash::initialize();
