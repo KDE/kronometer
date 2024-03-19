be3cef676d1fac730e12512c63e549cb599a9094
Friedrich W. H. Kossebau <kossebau@kde.org>
2024-03-19 21:48:59 +0100

    KLocalizedString::setApplicationDomain to be called post-QApp instance

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,12 @@
 
 int main (int argc, char **argv)
 {
-    KLocalizedString::setApplicationDomain("kronometer");
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
 #endif
     QApplication app {argc, argv};
+    KLocalizedString::setApplicationDomain("kronometer");
 
     KCrash::initialize();
 
